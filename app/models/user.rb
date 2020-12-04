8d07e4f59e59d0f35d7fa1a37200b28c2517976c
Ubuntu <ubuntu@ip-172-31-87-205.ec2.internal>
2020-12-04 09:48:37 +0000

    Finally

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
 validates :name, presence: true
-validates :age, presence: true       
+validates :age, presence: true 
+
+has_one_attached :avatar
 end
